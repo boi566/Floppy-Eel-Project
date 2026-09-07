37c7f161d7a9eda1c4b5a5fb2e3ab378677ab976
boi566 <84168449+boi566@users.noreply.github.com>
2026-09-07 18:50:55 +0530

    Add files via upload

---
+++ BigLeap3/floppyeelprototype6.py
@@ -0,0 +1,60 @@
+# drop your position in regular fen
+import chess as c
+d = input("enter a position: ")
+b = c.Board(d)
+VALS = {c.PAWN: 100 ,
+        c.BISHOP: 300,
+        c.KNIGHT: 300,
+        c.ROOK : 500,
+        c.QUEEN: 900,
+        c.KING: 0}
+def evl(b , depth):
+    if b.is_checkmate():
+        if b.turn == c.WHITE:
+                return(-1000000 - depth)
+        elif b.turn == c.BLACK:
+                return(+1000000 + depth)
+    elif b.is_game_over():
+        return(0)
+    score = 0
+    for i in b.piece_map().values():
+        if i.color == c.WHITE:
+            score += VALS[i.piece_type]
+        elif i.color == c.BLACK:
+            score -= VALS[i.piece_type]
+    return(score)
+def get(b , depth,
+alpha = float('-inf'),
+beta = float('inf')):
+    if depth == 0 or b.is_game_over():
+        return(evl(b , depth) , None)
+    if b.turn == c.WHITE:
+        maximum = float('-inf')
+        lowmove = None            
+        for move in b.legal_moves:
+            b.push(move)
+            score , _ = get(b , depth - 1 , alpha , beta)
+            b.pop()
+            if score > maximum:
+                maximum = score
+                lowmove = move
+                alpha = max(alpha , score)
+                if beta <= alpha:
+                    break
+        return(maximum , lowmove)
+    elif b.turn == c.BLACK:
+        minimum = float('inf')
+        lowmove = None
+        for move in b.legal_moves:
+            b.push(move)
+            score , _ = get(b , depth - 1 , alpha , beta)
+            b.pop()
+            if score < minimum:
+                minimum = score
+                lowmove = move
+                beta = min(beta , score)
+                if beta <= alpha:
+                    break
+        return(minimum , lowmove)
+print(get(b , 3))
+print(b)
